5b826080d7107d9bfa247142b0f15a25b152d6af
inspecthers <kg.geku@gmail.com>
2023-06-08 23:22:51 +0100

    fix wors page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -5,56 +5,241 @@ export default function page() {
         <h1 className="text-6xl font-bold">Work</h1>
       </section>
       <section className="bg-[#d6f4e2] p-20">
-        {/* [&>*:nth-child(odd)]:bg-red-500 [&>*:nth-child(even)]:bg-blue-500 */}
-        <div className="grid grid-cols-2 gap-10 [&>*:nth-child(odd)]:mt-10 [&>*:nth-child(even)]:mb-10">
-          <div className="border border-black bg-[url('/image.jpeg')]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            labore optio explicabo eos repudiandae ratione a tempora nisi quis
-            odio quas qui eveniet nostrum, sequi quod, mollitia, voluptate
-            minus. Hic.
-          </div>
-          <div className="border border-black">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            labore optio explicabo eos repudiandae ratione a tempora nisi quis
-            odio quas qui eveniet nostrum, sequi quod, mollitia, voluptate
-            minus. Hic.
-          </div>
-          <div className="border border-black">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            labore optio explicabo eos repudiandae ratione a tempora nisi quis
-            odio quas qui eveniet nostrum, sequi quod, mollitia, voluptate
-            minus. Hic.
-          </div>
-          <div className="border border-black">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            labore optio explicabo eos repudiandae ratione a tempora nisi quis
-            odio quas qui eveniet nostrum, sequi quod, mollitia, voluptate
-            minus. Hic.
-          </div>
-          <div className="border border-black bg-[url('/image.jpeg')]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            labore optio explicabo eos repudiandae ratione a tempora nisi quis
-            odio quas qui eveniet nostrum, sequi quod, mollitia, voluptate
-            minus. Hic.
-          </div>
-          <div className="border border-black">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            labore optio explicabo eos repudiandae ratione a tempora nisi quis
-            odio quas qui eveniet nostrum, sequi quod, mollitia, voluptate
-            minus. Hic.
-          </div>
-          <div className="border border-black">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            labore optio explicabo eos repudiandae ratione a tempora nisi quis
-            odio quas qui eveniet nostrum, sequi quod, mollitia, voluptate
-            minus. Hic.
-          </div>
-          <div className="border border-black">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            labore optio explicabo eos repudiandae ratione a tempora nisi quis
-            odio quas qui eveniet nostrum, sequi quod, mollitia, voluptate
-            minus. Hic.
-          </div>
+        <div className="grid grid-cols-2 gap-20 [&>*:nth-child(odd)]:mt-10 [&>*:nth-child(even)]:mb-10">
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="##"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-1.png')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 text-white">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    <p className="text-white text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p>
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="##"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-2.png')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 text-white">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    <p className="text-white text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p>
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="##"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-3.jpeg')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 text-white">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    <p className="text-white text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p>
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="##"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-4.jpeg')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 text-white">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    <p className="text-white text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p>
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="##"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-5.png')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 text-white">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    <p className="text-white text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p>
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="##"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-1.png')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 text-white">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    <p className="text-white text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p>
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
         </div>
       </section>
     </main>
