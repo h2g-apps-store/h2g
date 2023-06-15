781cfa77c941c4ff24f4b58b4a760783d5d86dbf
inspecthers <kg.geku@gmail.com>
2023-06-15 01:04:29 +0100

    header meni

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 export default function Header() {
   const pathname = usePathname();
   return (
-    <header className=" absolute w-full flex justify-between px-8 sm:px-24  text-black py-5 sm:py-10">
+    <header className="absolute w-full flex flex-col sm:flex-row justify-between sm:items-center sm:px-24  text-black  sm:py-10">
       <Link
         href="/"
         className={
@@ -25,7 +25,7 @@ export default function Header() {
               : "flex justify-between gap-10 text-[#aab5d0] text-2xl font-bold"
           }
         >
-          <li className="hover:underline underline-offset-8 decoration-4">
+          <li className="hover:underline underline-offset-8 decoration-4 text-xl">
             <Link href="/work">Work</Link>
           </li>
           <li className="hover:underline underline-offset-8 decoration-4">
@@ -35,6 +35,28 @@ export default function Header() {
             <Link href="/blog">Blog</Link>
           </li>
           <li className="hover:underline underline-offset-8 decoration-4">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className="sm:hidden absolute top-0 bg-gray-500 py-2 px-3 w-full">
+        <ul
+          className={
+            pathname == "/"
+              ? "flex justify-between gap-10 text-[#d8e3ff] text-2xl font-bold"
+              : "flex justify-between gap-10 text-[#aab5d0] text-2xl font-bold"
+          }
+        >
+          <li className="hover:underline underline-offset-8 decoration-4 text-xl">
+            <Link href="/work">Work</Link>
+          </li>
+          <li className="hover:underline underline-offset-8 decoration-4 text-xl">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="hover:underline underline-offset-8 decoration-4 text-xl">
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li className="hover:underline underline-offset-8 decoration-4 text-xl">
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
