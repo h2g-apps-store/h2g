72d7e104c6e733bcdaa22ecc68b07e557cf2af1d
inspecthers <kg.geku@gmail.com>
2023-06-21 21:55:06 +0100

    refact hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -10,9 +10,9 @@ const Hero = () => {
         <div
           data-aos="slide-left"
           data-aos-delay="1200"
-          className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
+          className="absolute h-full w-6/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
         >
-          <h1 className="rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
+          <h1 className="hidden rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
             {hero.firstName}{" "}
             <span className="text-dark_primary">{hero.LastName}</span>
           </h1>
@@ -22,7 +22,7 @@ const Hero = () => {
         <div className="pb-16 px-6 pt-5" data-aos="fade-down">
           <h2>{hero.title}</h2>
           <br />
-          <div className="flex justify-end">
+          <div className="hidden lg:flex justify-end">
             <button className="btn">{hero.btnText}</button>
           </div>
           <div className="flex flex-col gap-10 mt-10">
@@ -49,7 +49,7 @@ const Hero = () => {
             alt="img-cover"
             width={430}
             height={430}
-            className="h-full object-cover"
+            className="lg:h-full object-cover"
           />
         </div>
       </div>
