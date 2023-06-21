14b992796d46bcbaf96c640fe3a86b094dba6cac
inspecthers <kg.geku@gmail.com>
2023-06-21 21:42:41 +0100

    pages contact js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,20 @@ import Services from "@/components/sections/Services";
 import Skills from "@/components/sections/Skills";
 import Testimonials from "@/components/sections/Testimonials";
 
+// Animation package
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    Aos.init({
+      duration: 1800,
+      offset: 100,
+      disable: "mobile",
+    });
+  }, []);
+
   return (
     <main>
       <Navbar />
