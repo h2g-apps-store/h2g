de72ad398e4bafdfeb7990e6c5b568375f9ba8b0
inspecthers <kg.geku@gmail.com>
2023-07-01 16:32:45 +0100

    fix projecet section

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -20,11 +20,241 @@ const Projects = () => {
             {Projects.subtitle}
           </h4>
         </div>
-        <div className="border border-gray-300">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
-          laboriosam nulla adipisci, eligendi veniam tenetur magni hic possimus
-          blanditiis sint quia, dolor optio quam odit voluptas iusto
-          necessitatibus molestiae distinctio!
+        <div className="grid grid-cols-2 gap-10 sm:gap-20 [&>*:nth-child(odd)]:mt-10 [&>*:nth-child(even)]:mb-10">
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="/work"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-1.png')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="hidden sm:block relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 dark_primary">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    {/* <p className="dark_primary text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p> */}
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="/work"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-2.png')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="hidden sm:block relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 dark_primary">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    {/* <p className="dark_primary text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p> */}
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="/work"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-3.jpeg')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="hidden sm:block relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 dark_primary">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    {/* <p className="dark_primary text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p> */}
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="/work"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-4.jpeg')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="hidden sm:block relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 dark_primary">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    {/* <p className="dark_primary text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p> */}
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="/work"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-5.png')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="hidden sm:block relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 dark_primary">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    {/* <p className="dark_primary text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p> */}
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
+          <a
+            className="relative block w-full h-64
+                      bg-gray-900 group shadow-xl"
+            href="/work"
+          >
+            <div
+              className="absolute bg-[url('/projets/p-1.png')] bg-cover bg-center inset-0
+                            w-full h-64 group-hover:opacity-50"
+            ></div>
+            <div className="hidden sm:block relative p-10">
+              <div className="mt-2">
+                {/* Hidden content */}
+                <div
+                  className="transition-all transform
+                                translate-y-8 opacity-0
+                                group-hover:opacity-100
+                                group-hover:translate-y-0"
+                >
+                  <div className="p-2">
+                    <h1 className="text-4xl font-bold mb-2 dark_primary">
+                      Welcome to GeeksforGeeks.
+                    </h1>
+                    {/* <p className="dark_primary text-xl">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Ipsum distinctio et veritatis saepe tempora numquam
+                      quibusdam.
+                    </p> */}
+                    {/* <button
+                        className="px-4 py-2 text-sm
+                                            text-white bg-green-600"
+                      >
+                        Learn more
+                      </button> */}
+                  </div>
+                </div>
+                {/* End of hidden content */}
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     </section>
